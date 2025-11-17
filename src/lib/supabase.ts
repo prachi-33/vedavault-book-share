59d00c401b48a78d24b3c9bc174992271c224511
prachi-33 <prachisaran5@gmail.com>
2025-11-17 11:04:28 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://iyolobflagtoetgclicf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5b2xvYmZsYWd0b2V0Z2NsaWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzNTM0NzYsImV4cCI6MjA0OTkyOTQ3Nn0.hKfOCPXjMbKJjOQcPkZfXJE-qjuWj6jl5TvSvWzSQxE';
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5b2xvYmZsYWd0b2V0Z2NsaWNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTcxOTkyOSwiZXhwIjoyMDY3Mjk1OTI5fQ.ANjWRuEXIIZ6HRAp9Iu8JsRKqoLw6J7mnsRs-GDWf8o";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
